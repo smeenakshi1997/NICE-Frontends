055c7dc5350e1504ad1c40f227de349bb5d5ce2b
smeenakshi1997 <smeenakshi1997@gmail.com>
2022-03-25 11:54:31 +0530

    Update App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -5,6 +5,10 @@ import Navbar from "./components/Navbar";
 import Dashboard from "./views/dashboard/Dashboard.js";
 const Profile = React.lazy(() => import("profile/Profile"));
 const Projects = React.lazy(() => import("projects/Projects"));
+const Button = React.lazy(() => import("projects/Button"));
+
+//Line 31: button names inside components'.js has /profile or /project
+//Line 21: Exposing the button directly without creating a route tag and exposing the button created using the react lazy call above
 
 function App() {
   return (
@@ -12,17 +16,23 @@ function App() {
       <Navbar />
       <React.Suspense fallback={<LoadingModules />}>
         <div className="container mx-auto px-4 mt-16">
+          <div>
+            <React.Suspense fallback='Loading Button'>
+            <Button />  
+            </React.Suspense>
+          </div>
           <Switch>
             <Route path="/" exact>
               <Dashboard />
             </Route>
-            <Route path="/profile" exact>
+            <Route path="/profile" exact> 
               <Profile />
             </Route>
             <Route path="/projects" exact>
               <Projects />
             </Route>
           </Switch>
+          
         </div>
       </React.Suspense>
     </BrowserRouter>
