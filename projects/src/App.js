a829f33080e13d1ebb20c65e19ab485c9d749994
smeenakshi1997 <smeenakshi1997@gmail.com>
2022-03-25 11:57:46 +0530

    Update App.js

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+//Line 10&17: Button hidden in call from dashboard. Can be included within react.suspense if needed. Available in its own port.
 import Projects from "./views/projects/Projects";
+import Button from "./views/projects/Button";
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
+      <h1>Projects</h1>
+      <Button>
+        {' '}
+        Projects Button
+      </Button>
+      <BrowserRouter>
       <React.Suspense fallback={<LoadingModules />}>
         <Switch>
           <Route path="/" exact>
@@ -14,6 +22,7 @@ function App() {
         </Switch>
       </React.Suspense>
     </BrowserRouter>
+    </div>
   );
 }
 
